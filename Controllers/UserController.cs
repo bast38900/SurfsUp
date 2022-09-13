c28f5b4f4d142f0c2bc91d7905cac7c94bfd47e1
Bjarke Steffensen <bast38900@edu.ucl.dk>
2022-09-13 16:57:57 +0200

    ..

---
+++ Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SurfsUp.Models;
+
+namespace SurfsUp.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+
+        public UserController(UserManager<IdentityUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser appUser = new IdentityUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+
+    }
+}
