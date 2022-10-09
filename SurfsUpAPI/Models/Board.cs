3ce1cb7966ba856483dafa5b167601d2cd69332d
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-10-09 15:50:21 +0200

    Added SurfsUpAPI

---
+++ SurfsUpAPI/Models/Board.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SurfsUpAPI.Models
+{
+    public enum BoardState
+    {
+        Available,
+        Rented
+    }
+
+    public class Board
+    {
+        public Guid BoardId { get; set; }
+
+        [StringLength(255, MinimumLength = 1)]
+        [Required]
+        public string? BoardName { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [StringLength(255, MinimumLength = 1)]
+        [Required]
+        public string? Type { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public double Thickness { get; set; }
+
+        public double Volume { get; set; }
+
+        public BoardState State { get; set; } = BoardState.Available;
+
+        private string? picture;
+        public string? Picture
+        {
+            get { return picture; }
+            set { picture = value ?? ""; }
+        }
+
+        private string? equipment;
+        public string? Equipment
+        {
+            get { return equipment; }
+            set { equipment = value ?? ""; }
+        }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
