f580817545a25871b0cbf6b343c84a79559e4b81
bast38900 <91531850+bast38900@users.noreply.github.com>
2022-10-12 20:36:38 +0200

    Revert "Oskar"

--- SurfsUpAPI/Models/RentDto.cs
+++ SurfsUpAPI/Models/RentDto.cs
@@ -5,6 +5,5 @@
         public Guid UserId { get; set; }
         public Guid BoardId { get; set; }
         public DateTime EndRent { get; set; }
-        public byte[] RowVersion { get; set; }
     }
 }
--- SurfsUpMVC/Models/RentDto.cs
+++ SurfsUpMVC/Models/RentDto.cs
@@ -7,6 +7,5 @@ namespace SurfsUpMVC.Models
         public Guid UserId { get; set; }
         public Guid BoardId { get; set; }
         public DateTime EndRent { get; set; }
-        public byte[] RowVersion { get; set; }
     }
 }
