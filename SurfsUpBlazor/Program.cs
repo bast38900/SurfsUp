16ace840a0dfc98139be3e5bba3b126b1c8bdcd5
Mads Friis <madsfriis91@gmail.com>
2022-11-09 08:28:14 +0100

    Mangler at vide specifike dage.

--- SurfsUpBlazor/Program.cs
+++ SurfsUpBlazor/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SurfsUpBlazor;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddOidcAuthentication(options =>
 {
     // Configure your authentication provider options here.
