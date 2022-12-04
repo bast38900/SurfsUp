6cdfc1f46e8088c3d56cebe685fe94ffa6de93ec
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2022-12-04 22:16:36 +0100

    Added Scaffolding for ApplicationUser and added Address field in register form

--- SurfsUpWeb/Server/Data/Migrations/20221204202028_AddressFieldToUser.Designer.cs
+++ SurfsUpWeb/Server/Data/Migrations/20221204202028_AddressFieldToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurfsUpWeb.Server.Data;
 
@@ -11,9 +12,10 @@ using SurfsUpWeb.Server.Data;
 namespace SurfsUpWeb.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221204202028_AddressFieldToUser")]
+    partial class AddressFieldToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
